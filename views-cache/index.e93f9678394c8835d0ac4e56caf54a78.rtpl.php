3dd3cdc1318a8f4e517f98bfb01412d8421a70fb
EdlimaF <edlimaf@msn.com>
2018-10-11 23:46:43 -0300

    commit

---
+++ views-cache/index.e93f9678394c8835d0ac4e56caf54a78.rtpl.php
@@ -0,0 +1,111 @@
+<?php if(!class_exists('Rain\Tpl')){exit;}?>
+	<div class="slider-area">
+			<!-- Slider -->
+			<div class="block-slider block-slider4">
+				<ul class="" id="bxslider-home4">
+					<?php $counter1=-1;  if( isset($productsSlide) && ( is_array($productsSlide) || $productsSlide instanceof Traversable ) && sizeof($productsSlide) ) foreach( $productsSlide as $key1 => $value1 ){ $counter1++; ?>
+					<li>
+						<a title="Detalhes" href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><img src="<?php echo htmlspecialchars( $value1["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" alt="Slide" style="width: 35%; margin-left: 8%; margin-top: 10px;"></a>
+						<div class="caption-group">
+							<h3 class="caption title">
+								Aproveite 
+								<span class="primary">Oferta <strong>Especial</strong></span>
+							</h3>
+							<h4 class="caption subtitle"><?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>
+							<br>R$<?php echo formatPrice($value1["vlprice"]); ?> 
+							</h4>
+							<a class="caption button-radius" href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><span class="icon"></span>Detalhes</a>
+							<a class="caption button-radius" href="/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/add"><span class="icon"></span>Comprar</a>
+						</div>
+					</li>
+					<?php } ?>
+				</ul>
+			</div>
+			<!-- ./Slider -->
+	</div> <!-- End slider area -->
+
+	<div class="promo-area">
+		<div class="zigzag-bottom"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-3 col-sm-6">
+					<div class="single-promo promo1">
+						<i class="fa fa-refresh"></i>
+						<p>1 ano de garantia</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<div class="single-promo promo2">
+						<i class="fa fa-truck"></i>
+						<p>Frete grátis</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<div class="single-promo promo3">
+						<i class="fa fa-lock"></i>
+						<p>Pagamento seguro</p>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6">
+					<div class="single-promo promo4">
+						<i class="fa fa-gift"></i>
+						<p>Novos produtos</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div> <!-- End promo area -->
+
+	<div class="maincontent-area">
+		<div class="zigzag-bottom"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="latest-product">
+						<h2 class="section-title">Produtos</h2>
+						<div class="product-carousel">
+							<?php $counter1=-1;  if( isset($products) && ( is_array($products) || $products instanceof Traversable ) && sizeof($products) ) foreach( $products as $key1 => $value1 ){ $counter1++; ?>
+							<div class="single-product">
+								<div class="product-f-image">
+									<img src="<?php echo htmlspecialchars( $value1["desphoto"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" alt="">
+									<div class="product-hover">
+										<a href="/cart/<?php echo htmlspecialchars( $value1["idproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?>/add" class="add-to-cart-link"><i class="fa fa-shopping-cart"></i> Comprpr</a>
+										<a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>" class="view-details-link"><i class="fa fa-link"></i>Detalhes</a>
+									</div>
+								</div>
+
+								<h2><a href="/products/<?php echo htmlspecialchars( $value1["desurl"], ENT_COMPAT, 'UTF-8', FALSE ); ?>"><?php echo htmlspecialchars( $value1["desproduct"], ENT_COMPAT, 'UTF-8', FALSE ); ?></a></h2>
+
+								<div class="product-carousel-price">
+									<ins>R$<?php echo formatPrice($value1["vlprice"]); ?></ins>
+								</div>
+							</div>
+							<?php } ?>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div> <!-- End main content area -->
+
+	<div class="brands-area">
+		<div class="zigzag-bottom"></div>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-12">
+					<div class="brand-wrapper">
+						<div class="brand-list">
+							<img src="/res/site/img/brand1.png" alt="">
+							<img src="/res/site/img/brand2.png" alt="">
+							<img src="/res/site/img/brand3.png" alt="">
+							<img src="/res/site/img/brand4.png" alt="">
+							<img src="/res/site/img/brand5.png" alt="">
+							<img src="/res/site/img/brand6.png" alt="">
+							<img src="/res/site/img/brand1.png" alt="">
+							<img src="/res/site/img/brand2.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div> <!-- End brands area -->
